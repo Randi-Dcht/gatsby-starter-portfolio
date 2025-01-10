d497fbdf0351115bea49139f43efe9292bb743ed
Randi <56920756+Randi-Dcht@users.noreply.github.com>
2025-01-10 10:27:53 +0100

    update for me

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `RandyDev`,
+    siteTitleAlt: `RandyDev - Dev Portfolio`,
+    siteHeadline: `RandyDev - @Dochot.be`,
+    siteUrl: `https://dochot.be`,
+    siteDescription: `Portefolio de RandyDev sur les projets de developpement`,
     siteImage: `/banner.jpg`,
-    siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    siteLanguage: `fr`,
+    author: `@RandyDev`,
   },
   trailingSlash: `always`,
   plugins: [
